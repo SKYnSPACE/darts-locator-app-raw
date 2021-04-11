4c0ae020b28d6f53f602b9c2893a955453cd25cf
SKYnSPACE <skynspace@kaist.ac.kr>
2021-04-11 22:14:44 +0900

    Numeric, and dismissible keyboard.

--- src/screens/LocatorScreen.js
+++ src/screens/LocatorScreen.js
@@ -6,13 +6,27 @@ import {
   Button,
   TouchableOpacity,
   StyleSheet,
+  Keyboard,  TouchableWithoutFeedback
 } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const BORDER_COLOR = "#7f8fa6";
 
+const DismissKeyboard = ({ children }) => (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      {children}
+    </TouchableWithoutFeedback>
+  );
+
 function LocatorScreen({ navigation }) {
-  const [text, setText] = useState("");
+  const [lat, setLat] = useState("");
+  const [latFine, setLatFine] = useState("");
+  const [lon, setLon] = useState("");
+  const [lonFine, setLonFine] = useState("");
+  const [gzd, setGzd] = useState("");
+  const [gsid, setGsid] = useState("");
+  const [easting, setEasting] = useState("");
+  const [northing, setNorthing] = useState("");
 
   return (
     <View style={styles.containerStyle}>
@@ -20,49 +34,80 @@ function LocatorScreen({ navigation }) {
         <View style={styles.titleBox}>
           <Text style={styles.latLonTitle}>Latitude / Longitude</Text>
         </View>
-
+        <DismissKeyboard>
         <View style={styles.latBox}>
-            <Text style={styles.subTitle}>Latitude: </Text>
+          <Text style={styles.subTitle}>Latitude: </Text>
 
-            <View style={styles.latLonInputs}>
+          <View style={styles.latLonInputs}>
             <TextInput
-                style={{width: 30, height: 25, color: 'white', borderBottomWidth: 1, borderColor: 'white'}}
-                onChangeText={(text) => setText(text)}
-                defaultValue={text}
-                textAlign={'right'}
-                maxLength = {2}
+              style={{
+                width: 30,
+                height: 20,
+                color: "white",
+                borderBottomWidth: 1,
+                borderColor: "white",
+              }}
+              onChangeText={(lat) => setLat(lat)}
+              defaultValue={lat}
+              keyboardType="number-pad"
+              textAlign={"right"}
+              maxLength={2}
+            /> 
+            <Text style={styles.subTitle}>.</Text>
+            <TextInput
+              style={{
+                width: 70,
+                height: 20,
+                color: "white",
+                borderBottomWidth: 1,
+                borderColor: "white",
+              }}
+              onChangeText={(latFine) => setLatFine(latFine)}
+              defaultValue={latFine}
+              keyboardType="number-pad"
+              maxLength={7}
+            />
+          </View>
+        </View>
+        </DismissKeyboard>
+
+        <DismissKeyboard>
+        <View style={styles.lonBox}>
+          <Text style={styles.subTitle}>Longitude: </Text>
+
+          <View style={styles.latLonInputs}>
+            <TextInput
+              style={{
+                width: 30,
+                height: 20,
+                color: "white",
+                borderBottomWidth: 1,
+                borderColor: "white",
+              }}
+              onChangeText={(lon) => setLon(lon)}
+              defaultValue={lon}
+              keyboardType="number-pad"
+              textAlign={"right"}
+              maxLength={3}
             />
             <Text style={styles.subTitle}>.</Text>
             <TextInput
-                style={{ width: 70, height: 25, color: 'white', borderBottomWidth: 1,borderColor: 'white' }}
-                onChangeText={(text) => setText(text)}
-                defaultValue={text}
-                maxLength = {7}
+              style={{
+                width: 70,
+                height: 20,
+                color: "white",
+                borderBottomWidth: 1,
+                borderColor: "white",
+              }}
+              onChangeText={(lonFine) => setLonFine(lonFine)}
+              defaultValue={lonFine}
+              keyboardType="number-pad"
+              maxLength={7}
             />
-            </View>
+          </View>
         </View>
-
-        <View style={styles.lonBox}>
-            <Text style={styles.subTitle}>Longitude: </Text>
-
-            <View style={styles.latLonInputs}>
-            <TextInput
-                style={{ width: 30, height: 25, color: 'white', borderBottomWidth: 1,borderColor: 'white' }}
-                onChangeText={(text) => setText(text)}
-                defaultValue={text}
-                textAlign={'right'}
-                maxLength = {3}
-            />
-            <Text style={{ color: "white" }}>.</Text>
-            <TextInput
-                style={{ width: 70, height: 25, color: 'white', borderBottomWidth: 1,borderColor: 'white' }}
-                onChangeText={(text) => setText(text)}
-                defaultValue={text}
-                maxLength = {7}
-            />
-            </View>
-        </View>
-
+        </DismissKeyboard>
+        
       </View>
 
       <View style={styles.converterButtonBox}>
@@ -88,67 +133,101 @@ function LocatorScreen({ navigation }) {
         </TouchableOpacity>
       </View>
 
-
-
-
       <View style={styles.mgrsBox}>
+      <DismissKeyboard>
         <View style={styles.titleBox}>
           <Text style={styles.latLonTitle}>MGRS</Text>
         </View>
+        </DismissKeyboard>
 
+        <DismissKeyboard>
         <View style={styles.gzdBox}>
-            <Text style={styles.subTitle}>Grid Zone Designator: </Text>
-            <View style={styles.mgrsInputs}>
+          <Text style={styles.subTitle}>Grid Zone Designator: </Text>
+          <View style={styles.mgrsInputs}>
             <TextInput
-                style={{ width: 30, height: 25, color: 'white', borderBottomWidth: 1,borderColor: 'white' }}
-                onChangeText={(text) => setText(text)}
-                defaultValue={text}
-                textAlign={'center'}
-                maxLength = {2}
+              style={{
+                width: 30,
+                height: 25,
+                color: "white",
+                borderBottomWidth: 1,
+                borderColor: "white",
+              }}
+              onChangeText={(gzd) => setGzd(gzd)}
+              defaultValue={gzd}
+              textAlign={"center"}
+              maxLength={2}
             />
-            </View>
+          </View>
         </View>
+        </DismissKeyboard>
 
+        <DismissKeyboard>
         <View style={styles.gsidBox}>
-            <Text style={styles.subTitle}>100km Grid Square ID: </Text>
-            <View style={styles.mgrsInputs}>
+          <Text style={styles.subTitle}>100km Grid Square ID: </Text>
+          <View style={styles.mgrsInputs}>
             <TextInput
-                style={{ width: 30, height: 25, color: 'white', borderBottomWidth: 1,borderColor: 'white' }}
-                onChangeText={(text) => setText(text)}
-                defaultValue={text}
-                textAlign={'center'}
-                maxLength = {2}
+              style={{
+                width: 30,
+                height: 25,
+                color: "white",
+                borderBottomWidth: 1,
+                borderColor: "white",
+              }}
+              onChangeText={(gsid) => setGsid(gsid)}
+              defaultValue={gsid}
+              textAlign={"center"}
+              maxLength={2}
             />
-            </View>
+          </View>
         </View>
+        </DismissKeyboard>
+        <DismissKeyboard>
         <View style={styles.eastingBox}>
-            <Text style={styles.subTitle}>Easting: </Text>
-            <View style={styles.mgrsInputs}>
+          <Text style={styles.subTitle}>Easting: </Text>
+          <View style={styles.mgrsInputs}>
             <TextInput
-                style={{ width: 50, height: 25, color: 'white', borderBottomWidth: 1,borderColor: 'white' }}
-                onChangeText={(text) => setText(text)}
-                defaultValue={text}
-                textAlign={'center'}
-                maxLength = {5}
+              style={{
+                width: 50,
+                height: 25,
+                color: "white",
+                borderBottomWidth: 1,
+                borderColor: "white",
+              }}
+              onChangeText={(easting) => setEasting(easting)}
+              defaultValue={easting}
+              keyboardType="number-pad"
+              textAlign={"center"}
+              maxLength={5}
             />
-            </View>
+          </View>
         </View>
+        </DismissKeyboard>
+        <DismissKeyboard>
         <View style={styles.northingBox}>
-            <Text style={styles.subTitle}>Northing: </Text>
-            <View style={styles.mgrsInputs}>
+          <Text style={styles.subTitle}>Northing: </Text>
+          
+          <View style={styles.mgrsInputs}>
+          
             <TextInput
-                style={{ width: 50, height: 25, color: 'white', borderBottomWidth: 1,borderColor: 'white' }}
-                onChangeText={(text) => setText(text)}
-                defaultValue={text}
-                textAlign={'center'}
-                maxLength = {5}
+              style={{
+                width: 50,
+                height: 25,
+                color: "white",
+                borderBottomWidth: 1,
+                borderColor: "white",
+              }}
+              onChangeText={(northing) => setNorthing(northing)}
+              defaultValue={northing}
+              keyboardType="number-pad"
+              textAlign={"center"}
+              maxLength={5}
             />
-            </View>
+            
+          </View>
+          
         </View>
+        </DismissKeyboard>
       </View>
-
-
-
 
       <View style={styles.confirmButtonBox}>
         <TouchableOpacity
@@ -185,46 +264,42 @@ const styles = StyleSheet.create({
     height: 100,
   },
   latLonBox: {
-      padding: 10,
+    padding: 10,
     borderWidth: 1,
     borderColor: BORDER_COLOR,
     borderRadius: 5,
     flex: 4,
     marginHorizontal: 10,
   },
-  latBox:{
-    flexDirection: 'row',
-    alignItems: 'baseline'
+  latBox: {
+    flexDirection: "row",
   },
-  lonBox:{
-    flexDirection: 'row',
-    alignItems: 'baseline'
+  lonBox: {
+    flexDirection: "row",
   },
-  titleBox:{
-      alignItems:"center"
+  titleBox: {
+    alignItems: "center",
   },
   latLonTitle: {
-      color:'white',
-      fontSize: 20,
+    color: "white",
+    fontSize: 20,
   },
   subTitle: {
-        color: "white", 
-        fontSize: 18,
+    color: "white",
+    fontSize: 18,
   },
-  latLonInputs:{
-      height: 30,
-      flexDirection: 'row',
-      alignItems:'baseline'
+  latLonInputs: {
+    height: 30,
+    flexDirection: "row",
   },
   converterButtonBox: {
-    // borderWidth: 1,
     borderColor: BORDER_COLOR,
     flex: 1,
-    
+
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: 'center',
-    margin:10,
+    justifyContent: "center",
+    margin: 10,
   },
   converterButton: {
     backgroundColor: "gray",
@@ -235,7 +310,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
 
     borderRadius: 5,
-    margin : 30,
+    margin: 30,
   },
 
   mgrsBox: {
@@ -246,37 +321,31 @@ const styles = StyleSheet.create({
     flex: 7,
     marginHorizontal: 10,
   },
-  mgrsInputs:{
+  mgrsInputs: {
     height: 30,
-    flexDirection: 'row',
-    alignItems:'baseline'
-},
-  gzdBox:{
-    flexDirection: 'row',
-    alignItems: 'baseline'
+    flexDirection: "row",
   },
-  gsidBox:{
-    flexDirection: 'row',
-    alignItems: 'baseline'
+  gzdBox: {
+    flexDirection: "row",
   },
-  eastingBox:{
-    flexDirection: 'row',
-    alignItems: 'baseline'
+  gsidBox: {
+    flexDirection: "row",
   },
-  northingBox:{
-    flexDirection: 'row',
-    alignItems: 'baseline'
+  eastingBox: {
+    flexDirection: "row",
+  },
+  northingBox: {
+    flexDirection: "row",
   },
 
   confirmButtonBox: {
-    // borderWidth: 1,
     borderColor: BORDER_COLOR,
     flex: 1,
     marginHorizontal: 10,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    margin:10,
+    margin: 10,
   },
   confirmButton: {
     backgroundColor: "skyblue",
